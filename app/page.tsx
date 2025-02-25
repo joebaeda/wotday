b1c6176f7cf2dcf65ee0c4138d878e65817a5bc1
Joe Bae <lineanity@gmail.com>
2025-02-25 09:56:13 +0700

    minor fix

--- app/page.tsx
+++ app/page.tsx
@@ -111,6 +111,7 @@ export default function Home() {
     if (isConfirmed && isCastSuccess) {
       setWordsText("");
       setIsCastSuccess(false);
+      setShowForm(false);
     }
   }, [isCastSuccess, isConfirmed]);
 
